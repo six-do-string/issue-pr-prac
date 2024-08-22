a342da0d72a6f8f0dad3139db4ce83d279363149
JeongSeok <ljs0429777@gmail.com>
2024-08-22 16:00:22 +0900

    Feat: 연습용 프로젝트 생성

---
+++ src/test/java/com/est/issueprprac/IssuePrPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.est.issueprprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuePrPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
